41ff8667ce3ab96fcc6907554903cf3c93f31e36
huskycgn <jjslappn!8>
2023-11-07 19:15:00 +0100

    Initial commit

---
+++ funcs.py
@@ -0,0 +1,45 @@
+import requests
+import telebot
+from cred import *
+
+###TEST URL###
+BASE_URL = 'https://test.api.amadeus.com/'
+
+###PROD URL###
+# BASE_URL = 'https://api.amadeus.com/'
+
+
+def get_access_token():
+    body = {"grant_type": "client_credentials",
+            "client_id": API_KEY,
+            "client_secret": API_SEC
+            }
+    token = requests.request(method='post', url=f'{BASE_URL}v1/security/oauth2/token', data=body)
+    access_token = token.json()['access_token']
+    return access_token
+
+
+def get_flight_data(origin: type(str),
+                    destination: type(str),
+                    depdate: type(str),
+                    returndate: type(str),
+                    pax: type(int),
+                    bookclass: type(str)):
+    auth_header = {'Authorization': f'Bearer {get_access_token()}'}
+    flight_data = requests.request(method='get',
+                                   url=f'{BASE_URL}v2/shopping/flight-offers?originLocationCode'
+                                       f'={origin}'
+                                       f'&destinationLocationCode={destination}'
+                                       f'&departureDate={depdate}'
+                                       f'&returnDate={returndate}'
+                                       f'&adults={pax}'
+                                       f'&travelClass={bookclass}'
+                                       f'&nonStop=true',
+                                   headers=auth_header)
+
+    return flight_data.json()
+
+
+def send_telegram(message):
+    bot = telebot.TeleBot(token=TELE_TOKEN)
+    bot.send_message(chat_id=TELE_CHAT_ID, text=message)
---
+++ main.py
@@ -0,0 +1,58 @@
+from funcs import *
+
+
+class Flight:
+    def __init__(self, origin, destination, depdate, retdate, carrier, price):
+        self.origin = origin
+        self.destination = destination
+        self.depdate = depdate
+        self.retdate = retdate
+        self.carrier = carrier
+        self.price = '{:,}'.format(float(price))
+        # price in EUR
+
+
+###
+# flight data
+PAX = 2
+DEP = 'FRA'
+DEP_DATE = '2024-06-01'
+ARR = 'DFW'
+RET_DATE = '2024-06-30'
+CLASS = 'PREMIUM_ECONOMY'
+###
+
+rawdata = get_flight_data(origin=DEP,
+                          destination=ARR,
+                          depdate=DEP_DATE,
+                          returndate=RET_DATE,
+                          pax=PAX,
+                          bookclass=CLASS)
+
+offers = rawdata['data']
+
+
+flightlist = []
+
+for i in offers:
+    flightlist.append(Flight(
+        origin=i['itineraries'][0]['segments'][0]['departure']['iataCode'],
+        destination=i['itineraries'][1]['segments'][0]['departure']['iataCode'],
+        depdate=i['itineraries'][0]['segments'][0]['departure']['at'],
+        retdate=i['itineraries'][1]['segments'][0]['departure']['at'],
+        carrier=i['itineraries'][0]['segments'][0]['operating']['carrierCode'],
+        price=i['price']['total']))
+
+# print(flightlist[0].carrier)
+
+for f in flightlist:
+
+    output = (f"*********\n"
+              f"Flug {f.origin} -> {f.destination} am\n{f.depdate}\nmit {f.carrier} "
+              f"kostet derzeit EUR {f.price} für {PAX} Personen."
+              f"\n"
+              f"*********")
+
+    print(output)
+
+    send_telegram(output)
